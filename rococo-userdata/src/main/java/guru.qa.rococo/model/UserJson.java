8db24bb2f1a91a3a6c27db3e359e78cb9f958f52
Darya Chibisova <darya.chibisova@raiffeisen.ru>
2024-04-01 13:37:16 +0300

    fix userdata

--- rococo-userdata/src/main/java/guru.qa.rococo/model/UserJson.java
+++ rococo-userdata/src/main/java/guru.qa.rococo/model/UserJson.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import guru.qa.rococo.data.UserEntity;
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
-import rococo_userdata.User;
 
 
 import java.nio.charset.StandardCharsets;
@@ -25,29 +24,6 @@ public record UserJson(
     //добавлять картины/музеи/художников
 {
 
-  //???
-
-  public @Nonnull
-  User toJaxbUser() {
-    User jaxbUser = new User();
-    jaxbUser.setId(id != null ? id.toString() : null);
-    jaxbUser.setUsername(username);
-    jaxbUser.setFirstname(firstname);
-    jaxbUser.setLastname(lastname);
-    jaxbUser.setAvatar(avatar);
-    return jaxbUser;
-  }
-
-  public static @Nonnull UserJson fromJaxb(@Nonnull User jaxbUser) {
-    return new UserJson(
-        jaxbUser.getId() != null ? UUID.fromString(jaxbUser.getId()) : null,
-        jaxbUser.getUsername(),
-        jaxbUser.getFirstname(),
-        jaxbUser.getLastname(),
-        jaxbUser.getAvatar()
-
-    );
-  }
 
   public static @Nonnull UserJson fromEntity(@Nonnull UserEntity entity) {
     return new UserJson(
